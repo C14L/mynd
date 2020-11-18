fc256e50ca163bb648a67a6108192c2deff41f63
Christian Steimel <me@C14L.com>
2020-11-18 21:00:25 +0100

    Config scripts

---
+++ .upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+[ -z "$SERVER01" ] && echo "Error: SERVER01 envvar not set." && exit 1
+
+SRC=$( cd "$( dirname "$0" )"; pwd )
+DST="$SERVER01:/opt/mynd/"
+
+echo "${SRC} >>> ${DST}"
+
+rsync -rtvP --delete \
+    --exclude=*.log \
+    --exclude=db.sqlite3 \
+    --exclude=**/__pycache__ \
+    ${SRC} ${DST}
+
+pass webdev/server01-chris | \
+    head -n1 | \
+    ssh -tt ${SERVER01} "sudo systemctl restart mynd.service"
